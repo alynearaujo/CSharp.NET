eb59fd665bc5c4475125f28eefc927432e4e060b
Alyne Araujo <alyne.arauujo@gmail.com>
2024-04-12 00:23:24 -0300

    operador de atribuição e conversão de tipo

---
+++ 1.Módulo/Tipos_de_Operadores/Program.cs
@@ -0,0 +1,34 @@
+﻿// Tipos de operadores
+// atribuição (=)
+
+// int a = 10;
+// int b = 20;
+// int c = a + b;
+
+// c += 5;  
+// Console.WriteLine(c);
+
+// c -= 5;
+// Console.WriteLine(c);
+
+// c *= 5;
+// Console.WriteLine(c);
+
+// Convertendo um tipo string para inteiro | existem duas formas: 
+
+// int a = Convert.ToInt32("5");
+// Console.WriteLine(a);
+
+// int a = int.Parse("5");
+// Console.WriteLine(a);
+
+// int b = Convert.ToInt32(null);
+// Console.WriteLine($"{a}, {b}");
+
+//Convertendo um tipo inteiro para string 
+
+int c = 32;
+string d = c.ToString();
+Console.WriteLine(d);
+
+
