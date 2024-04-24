4f1bfa70ce61f35a1c37f3cac0164dd282f38760
Alyne Araujo <alyne.arauujo@gmail.com>
2024-04-23 23:50:45 -0300

    Entendendo Solution

---
+++ 1.Módulo/Solution/Estrturura_de_Repetição_Cópia/Program.cs
@@ -0,0 +1,35 @@
+﻿// FOR
+// int numero = 5;
+
+// for (int contador = 0; contador <= 10; contador ++)
+// {
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+// }
+
+// WHILE
+//  int numero = 5;
+//  int contador = 0;
+
+//  while (contador <= 10)
+//  {
+//      Console.WriteLine($"{contador}º Execução: {numero} x {contador} = {numero * contador}");
+//        contador++;
+
+//      if (contador == 6)
+//      {
+//          break;
+//      }
+//  }
+
+//DO WHILE
+ int soma = 0, numero = 0;
+
+ do
+ {
+     Console.WriteLine("Digite um número (0 para parar).");
+     numero = Convert.ToInt32(Console.ReadLine());
+     soma += numero;
+        
+ } while (numero != 0);
+
+ Console.WriteLine($"Total da soma dos números digitados é: {soma}");
--- 1.Módulo/Tipos_de_Operadores/obj/Debug/net8.0/Tipos_de_Operadores.AssemblyInfo.cs
+++ 1.Módulo/Tipos_de_Operadores/obj/Debug/net8.0/Tipos_de_Operadores.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tipos_de_Operadores")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17e5a6e962c036de0f09cb87acfba9ad32c3399f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a534aba13b16978c4122e41336883a26133b6b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tipos_de_Operadores")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tipos_de_Operadores")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
