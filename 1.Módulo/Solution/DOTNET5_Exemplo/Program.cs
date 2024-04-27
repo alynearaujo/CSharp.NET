48f22ec55866921ad99172ad27a5c9dd306fef72
Alyne Araujo <alyne.arauujo@gmail.com>
2024-04-26 21:44:42 -0300

    estrutura do program em DOTNET5 e DOTNET6

---
+++ 1.Módulo/Solution/DOTNET5_Exemplo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DOTNET5_Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
