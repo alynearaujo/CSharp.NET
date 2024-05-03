278a1e63e5f3e0ec9416ea0624a71bbd6b1a3c73
Alyne Araujo <alyne.arauujo@gmail.com>
2024-05-02 22:43:58 -0300

    array

--- 1.Módulo/Array_e_Listas/Program.cs
+++ 1.Módulo/Array_e_Listas/Program.cs
@@ -1,21 +1,41 @@
 ﻿
-int[] arrayInteiros = new int[3];
-arrayInteiros[0] = 72;
-arrayInteiros[1] = 64;
-arrayInteiros[2] = 50;
+List<string> listaString = new List<string>(); 
 
-Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+listaString.Add("PE");
+listaString.Add("BA");
+listaString.Add("RN");
 
-Console.WriteLine("Percorrendo o Array com o FOR");
-for (int contador = 0; contador < arrayInteiros.Length; contador++)
+
+Console.WriteLine("Percorrendo a lista com FOR");
+for(int contador = 0; contador < listaString.Count; contador++)
 {
-    Console.WriteLine($"Posição Nº {contador} - {arrayInteiros[contador]}");
+    Console.WriteLine($"Posição Nº {contador} - {listaString[contador]}");
+} 
+
+
+Console.WriteLine("Percorrendo a lista com FOREACH");
+int contadorForeach = 0;
+foreach(string item in listaString)
+{
+    Console.WriteLine($"Posição Nº {contadorForeach} - {item}");
 }
 
 
+// int[] arrayInteiros = new int[3];
+// arrayInteiros[0] = 72;
+// arrayInteiros[1] = 64;
+// arrayInteiros[2] = 50;
 
+// int[] arrayInteirosDobrado = new int[arrayInteiros.Length * 2];
+// Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
 
+// Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
 
+// Console.WriteLine("Percorrendo o Array com o FOR");
+// for (int contador = 0; contador < arrayInteiros.Length; contador++)
+// {
+//     Console.WriteLine($"Posição Nº {contador} - {arrayInteiros[contador]}");
+// }
 
 // Console.WriteLine("Percorrendo o Array com FOREACH");
 // foreach (int valor in arrayInteiros)
--- 1.Módulo/Array_e_Listas/obj/Debug/net8.0/Array_e_Listas.AssemblyInfo.cs
+++ 1.Módulo/Array_e_Listas/obj/Debug/net8.0/Array_e_Listas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Array_e_Listas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ff2d280aa2e6114a7473f412050015aeace3e8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ccdae50d5afbb753e6a6a476455d9a89d3d95d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Array_e_Listas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Array_e_Listas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
