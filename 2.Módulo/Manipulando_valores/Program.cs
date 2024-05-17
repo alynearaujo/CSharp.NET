db5f354d5a1c4bc0cffda5dd8a25733c50e08b77
alyne <alyne.arauujo@gmail.com>
2024-05-16 22:06:56 -0300

    formatando valores monetários

--- 2.Módulo/Manipulando_valores/Program.cs
+++ 2.Módulo/Manipulando_valores/Program.cs
@@ -1,14 +1,35 @@
 ﻿using Manipulando_valores.Models;
+using System.Globalization;
 
-Pessoa p1 = new Pessoa(nome: "Alane", sobrenome: "Vitória");
-Pessoa p2 = new Pessoa(nome: "Alyne", sobrenome: "Rodrigues");
-Pessoa p3 = new Pessoa(nome: "Luna", sobrenome: "Rodrigues");
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR"); // alterando a localização do código
 
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Ingles";
-cursoDeIngles.Alunos = new List<Pessoa>();
+decimal valorMonetario = 1582.40M;
+Console.WriteLine($"{valorMonetario:C}");
+Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US"))); // alterando a localização da cultura
 
-cursoDeIngles.AdicionarAluno(p1);
-cursoDeIngles.AdicionarAluno(p2);
-cursoDeIngles.AdicionarAluno(p3);
-cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa(nome: "Alane", sobrenome: "Vitória");
+// Pessoa p2 = new Pessoa(nome: "Alyne", sobrenome: "Rodrigues");
+// Pessoa p3 = new Pessoa(nome: "Luna", sobrenome: "Rodrigues");
+
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.Nome = "Ingles";
+// cursoDeIngles.Alunos = new List<Pessoa>();
+
+// cursoDeIngles.AdicionarAluno(p1);
+// cursoDeIngles.AdicionarAluno(p2);
+// cursoDeIngles.AdicionarAluno(p3);
+// cursoDeIngles.ListarAlunos();
